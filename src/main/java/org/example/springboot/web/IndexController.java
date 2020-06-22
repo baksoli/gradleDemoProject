725035a1a2abb6caa419db2019938409b1231a1f
parksoli <baksoli@genieworks.net>
2020-06-22 18:06:27 +0900

    구글/네이버로 로그인 하기, 테스트코드적용

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.config.auth.LoginUser;
+import org.example.springboot.config.auth.dto.SessionUser;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,17 +10,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    //private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         //musteche starter(spring-boot-starter-mustache)가 있어
         //앞의 경로와 뒤의 파일 확장자가 자동으로 지정.
 
         model.addAttribute("posts", postsService.findAllDesc());
+
+        // 구글계정으로 로그인
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("uName",user.getName());
+        }
 
         return "index";
     }
--- src/test/java/org/example/springboot/HelloControllerTest.java
+++ src/test/java/org/example/springboot/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package org.example.springboot;
 
+import org.example.springboot.config.auth.SecurityConfig;
 import org.example.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -16,7 +20,10 @@ import static org.hamcrest.Matchers.is;
 // SpringRunner라는 스프링 실행자를 사용, 스프링 부트 테스트와 JUnit사이에 연결자 역할
 @RunWith(SpringRunner.class)
 // Web(Spring MVC)에 집중할 수 있는 어노테이션
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters={
+        @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    })
 public class HelloControllerTest {
 
     // 스프링이 관리하는 빈을 주입
@@ -24,6 +31,7 @@ public class HelloControllerTest {
     private MockMvc mvc;
     // 웹 API 테스트시 사용, 스프링 MVC 테스트의 시작점
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello() throws Exception{
         String hello = "hello";
@@ -31,6 +39,7 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void return_helloDto() throws Exception{
         String name="hello";
